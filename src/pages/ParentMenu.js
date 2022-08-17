a92a80f756139701013c1a11a03a9605b0854ebd
TaLuKuTaY <talukutay@gmail.com>
2022-08-17 13:46:06 +0300

    parentpage created

---
+++ src/pages/ParentMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../styles/ParentMenu.css"
+const ParentMenu = () => {
+    return (
+        <div>
+          <h1 >Welcome to Crypto Legacy</h1>
+          <i class="fa-brands fa-ethereum"></i>
+<button className="button button1">Kids</button>
+<button className="button button2">Orders</button>
+<img src="https://icons.iconarchive.com/icons/cjdowner/cryptocurrency-flat/256/Ethereum-ETH-icon.png"  ></img>
+
+        </div>
+    );
+};
+
+export default ParentMenu;
